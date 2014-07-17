10e1a0614f0654f008ee3f4617e24a67d967bbb8
rbertagna <rbertagna@riverdale.edu>
2014-07-16 22:57:14 -0400

    Working but basic

--- check_for_change.rb
+++ check_for_change.rb
@@ -1,52 +1,53 @@
-
+require 'nokogiri'
+require 'open-uri'
+require 'twilio-ruby'
+require 'time'
 
 class Goal
 
-  def initialize
-    require 'nokogiri'
-    require 'open-uri'
-    require 'twilio-ruby'
+  def initialize    
     require_relative '/Users/julianne/Desktop/development/FINAL_project/scrape.rb'
-    Scrape.new #Without Object Orientation: can't find variables in scrape.rb
-    @Jozy = @stats[:Jozy_Stat] #With: Error: dynamic constant assignment (SyntaxError)
-    @Julian = @stats[:Julian_Stat] #Too Dynamic to name inside class and def
-    @Agudelo = @stats[:Agudelo_Stat]
-    @Boyd = @stats[:Boyd_Stat]
-    @Gedion = @stats[:Gedion_Stat]
-    @Yedlin = @stats[:Yedlin_Stat]
-    until @Jozy == 3107492
+    @goals = Scrape.new #Without Object Orientation: can't find variables in scrape.rb
+    @Jozy = @goals.call(:Jozy_Stat)#With: Error: dynamic constant assignment (SyntaxError)
+    @Julian = @goals.call(:Julian_Stat) #Too Dynamic to name inside class and def
+    @Agudelo = @goals.call(:Agudelo_Stat)
+    @Boyd = @goals.call(:Boyd_Stat)
+    @Gedion = @goals.call(:Gedion_Stat)
+    @Yedlin = @goals.call(:Yedlin_Stat)
+    until @Jozy == 'pizza'
+      @goals = Scrape.new
       change
     end
   end
 
   def message(name)
     require_relative 'twilio.rb'
-    Messenger.new('+13476758819', '#{name} scored a goal! GoLaZo!! #USMNT2018')
+    Messenger.new('+13476758819', '+13477845676', '+13108900142', '+13476758775', name + ' scored a goal! GoLaZo!! #USMNT2018')
   end
 
   def change
-    if @stats[:Jozy_Stat] != @Jozy
+    if @goals.call(:Jozy_Stat) != @Jozy
       message('Jozy Altidore')
-      @Jozy = @stats[:Jozy_Stat]
-    elsif @stats[:Julian_Stat] != @Julian
+      @Jozy = @goals.call(:Jozy_Stat)
+    elsif @goals.call(:Julian_Stat) != @Julian
       message('Julian Green')
-      @Julian = @stats[:Julian_Stat]
-    elsif @stats[:Agudelo_Stat] != @Agudelo
+      @Julian = @goals.call(:Julian_Stat)
+    elsif @goals.call(:Agudelo_Stat) != @Agudelo
       message('Juan Agudelo')
-      @Agudelo = @stats[:Agudelo_Stat]
-    elsif @stats[:Boyd_Stat] != @Boyd
+      @Agudelo = @goals.call(:Agudelo_Stat)
+    elsif @goals.call(:Boyd_Stat) != @Boyd
       message('Terrence Boyd')
-      @Boyd = @stats[:Boyd_Stat]
-    elsif @stats[:Gedion_Stat] != @Gedion
+      @Boyd = @goals.call(:Boyd_Stat)
+    elsif @goals.call(:Gedion_Stat) != @Gedion
       message('Gedion Zelalem')
-      @Gedion = @stats[:Gedion_Stat]
-    elsif @stats[:Yedlin_Stat] != @Yedlin
+      @Gedion = @goals.call(:Gedion_Stat)
+    elsif @goals.call(:Yedlin_Stat) != @Yedlin
       message('DeAndre Yedlin')
-      @Yedlin = @stats[:Yedlin_Stat]
+      @Yedlin = @goals.call(:Yedlin_Stat)
     else
       puts "No Goals to Report"
     end
-    sleep(300)
+    sleep(30)
   end
 
 
--- scrape.rb
+++ scrape.rb
@@ -5,24 +5,16 @@ require 'open-uri'
 class Scrape
 
   def initialize
+    htmls
+    @jozy = Nokogiri::HTML(@jozy_html)
+    @julian = Nokogiri::HTML(@julian_html)
+    @agudelo = Nokogiri::HTML(@agudelo_html)    
+    @boyd = Nokogiri::HTML(@boyd_html)
+    @gedion = Nokogiri::HTML(@gedion_html)
+    @yedlin = Nokogiri::HTML(@yedlin_html)
 
-    Jozy_html = open('http://en.wikipedia.org/wiki/Jozy_Altidore')
-    jozy = Nokogiri::HTML(Jozy_html)
-
-    Julian_html = open('http://en.wikipedia.org/wiki/Julian_Green')
-    julian = Nokogiri::HTML(Julian_html)
-
-    Agudelo_html = open('http://en.wikipedia.org/wiki/Juan_Agudelo')
-    agudelo = Nokogiri::HTML(Agudelo_html)
-
-    Boyd_html = open('http://en.wikipedia.org/wiki/Terrence_Boyd')
-    boyd = Nokogiri::HTML(Boyd_html)
-
-    Gedion_html = open('http://en.wikipedia.org/wiki/Gedion_Zelalem')
-    gedion = Nokogiri::HTML(Gedion_html)
-
-    Yedlin_html = open('http://en.wikipedia.org/wiki/DeAndre_Yedlin')
-    yedlin = Nokogiri::HTML(Yedlin_html)
+    scrape
+  end
     #Jozy_Stat: jozy.css(#mw-content-text > table:nth-child(51) > tbody > tr:nth-child(26) > th:nth-child(11))
     #Julian_Stat: julian.css(#mw-content-text > table:nth-child(21) > tbody > tr:nth-child(5) > th:nth-child(11))
     #Agudelo_Stat: agudelo.css(#mw-content-text > table:nth-child(31) > tbody > tr:nth-child(14) > th:nth-child(11))
@@ -30,16 +22,29 @@ class Scrape
     #Gedion_Stat: gedion.css(#mw-content-text > table.wikitable > tbody > tr:nth-child(4) > th:nth-child(11))
     #Yedlin_Stat: yedlin.css(#mw-content-text > table.wikitable > tbody > tr:nth-child(8) > th:nth-child(11))
 
-  
+  def htmls
+    @jozy_html = open('http://en.wikipedia.org/wiki/Jozy_Altidore')
+    @julian_html = open('http://en.wikipedia.org/wiki/Julian_Green')
+    @agudelo_html = open('http://en.wikipedia.org/wiki/Juan_Agudelo')
+    @boyd_html = open('http://en.wikipedia.org/wiki/Terrence_Boyd')
+    @gedion_html = open('http://en.wikipedia.org/wiki/Gedion_Zelalem')
+    @yedlin_html = open('http://en.wikipedia.org/wiki/DeAndre_Yedlin')
+  end
+
+  def scrape
     @stats = { #Everybody's career goals
       #mw-content-text > table:nth-child(51) > tbody > tr:nth-child(26) > th:nth-child(11)
-      :Jozy_Stat => jozy.css('#mw-content-text > table:nth-child(51) > tr:nth-child(26) > th:nth-child(11)').text,
-      :Julian_Stat => julian.css('#mw-content-text > table:nth-child(21) > tr:nth-child(5) > th:nth-child(11)').text,
-      :Agudelo_Stat => agudelo.css('#mw-content-text > table:nth-child(31) > tr:nth-child(14) > th:nth-child(11)').text,
-      :Boyd_Stat => boyd.css('#mw-content-text > table.wikitable > tr:nth-child(12) > th:nth-child(9)').text,
-      :Gedion_Stat => gedion.css('#mw-content-text > table.wikitable > tr:nth-child(4) > th:nth-child(11)').text,
-      :Yedlin_Stat => yedlin.css('#mw-content-text > table.wikitable > tr:nth-child(8) > th:nth-child(11)').text
+      :Jozy_Stat => @jozy.css('#mw-content-text > table:nth-child(51) > tr:nth-child(26) > th:nth-child(11)').text,
+      :Julian_Stat => @julian.css('#mw-content-text > table:nth-child(21) > tr:nth-child(5) > th:nth-child(11)').text,
+      :Agudelo_Stat => @agudelo.css('#mw-content-text > table:nth-child(31) > tr:nth-child(14) > th:nth-child(11)').text,
+      :Boyd_Stat => @boyd.css('#mw-content-text > table.wikitable > tr:nth-child(12) > th:nth-child(9)').text,
+      :Gedion_Stat => @gedion.css('#mw-content-text > table.wikitable > tr:nth-child(4) > th:nth-child(11)').text,
+      :Yedlin_Stat => @yedlin.css('#mw-content-text > table.wikitable > tr:nth-child(8) > th:nth-child(11)').text
     }
+  end
+
+  def call(key)
+    @stats[key]
   end
 
 end
