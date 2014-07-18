3c934def0a7f059a5eda3bec3599405aa77dcc21
rbertagna <rbertagna@riverdale.edu>
2014-07-18 14:39:25 -0400

    Change Website

--- app.rb
+++ app.rb
@@ -3,12 +3,22 @@ Bundler.require #require everything in bundler in gemfile
 require_relative './lib/twilio.rb'
 require_relative './lib/scrape.rb'
 require_relative './lib/check_for_change.rb'
+require_relative './lib/Data.rb'
 
 
 get '/' do
-  @change_goals = Goal.new
-  @change_goals.change_for_view
-  @message = @change_goals.call_message
-  @time = @change_goals.call_time
+  @posts = Web.new
+  @jozy = @posts.jozy
+  @julian =  @posts.julian
+  @agudelo =  @posts.agudelo
+  @boyd =  @posts.boyd
+  @gedion =  @posts.gedion
+  @yedlin =  @posts.yedlin
+  @gil =  @posts.gil
+  @aron =  @posts.aron
+  @nagbe =  @posts.nagbe
+  @corona =  @posts.corona
+  @bradley =  @posts.bradley
+  @trapp =  @posts.trapp
   erb :index #this tells your program to use the html associated with the index.html.erb file in your browser
 end 
---
+++ lib/Data.rb
@@ -0,0 +1,74 @@
+require 'nokogiri'
+require 'open-uri'
+require_relative 'scrape.rb'
+
+class Web
+
+  def initialize
+    @jozy = 75
+    @julian = 15
+    @agudelo = 22
+    @boyd = 72
+    @gedion = 0
+    @yedlin = 4
+    @gil = 12
+    @aron = 71
+    @nagbe = 26
+    @corona = 10
+    @bradley = 35
+    @trapp = 0
+    @goals = Scrape.new
+  end
+  def jozy
+    @jozy_diff = @goals.call(:Jozy_Stat).to_i - @jozy
+    @jozy_diff
+  end
+  def julian
+    @julian_diff = @goals.call(:Julian_Stat).to_i - @julian
+    @julian_diff
+  end
+  def agudelo
+    @agudelo_diff = @goals.call(:Agudelo_Stat).to_i - @agudelo
+    @agudelo_diff
+  end
+  def boyd
+    @boyd_diff = @goals.call(:Boyd_Stat).to_i - @boyd
+    @boyd_diff
+  end
+  def gedion
+    @gedion_diff = @goals.call(:Gedion_Stat).to_i - @gedion
+    @gedion_diff
+  end
+  def yedlin
+    @yedlin_diff = @goals.call(:Yedlin_Stat).to_i - @yedlin
+    @yedlin_diff
+  end
+  def gil
+    @gil_diff = @goals.call(:Gil_Stat).to_i - @gil
+    @gil_diff
+  end
+  def aron
+    @aron_diff = @goals.call(:Aron_Stat).to_i - @aron
+    @aron_diff
+  end
+  def nagbe
+    @nagbe_diff = @goals.call(:Nagbe_Stat).to_i - @nagbe
+    @nagbe_diff
+  end
+  def corona
+    @corona_diff = @goals.call(:Corona_Stat).to_i - @corona
+    @corona_diff
+  end
+  def bradley
+    @bradley_diff = @goals.call(:Bradley_Stat).to_i - @bradley
+    @bradley_diff
+  end
+  def trapp
+    @trapp_diff = @goals.call(:Trapp_Stat).to_i - @trapp
+    @trapp_diff
+  end
+
+end
+
+cmon = Web.new
+puts cmon.corona
