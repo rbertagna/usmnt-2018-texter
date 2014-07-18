cb6be6a04c1285dbc956c26ba03e64e9c5cca776
rbertagna <rbertagna@riverdale.edu>
2014-07-18 12:24:40 -0400

    trying to fix link

--- lib/check_for_change.rb
+++ lib/check_for_change.rb
@@ -26,7 +26,7 @@ class Goal
 
   def message(name)
     @message = name + ' scored a goal! GoLaZo!! #USMNT2018'
-    Messenger.new('+13476758819', '+17187152680', @message)
+    Messenger.new('+13476758819', '+17187152680','19175769322', @message)
     puts @message
     @time = Time.now
     puts Time.now
